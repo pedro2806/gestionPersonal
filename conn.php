67b68c695bca67fe0679a0936d32915dba1d5bc5
pedro2806 <isc.pedro.mtz@gmail.com>
2026-05-29 13:29:55 -0600

    Delete conn.php

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-// conn.php - Conexión unificada para base de datos mess_rrhh
-$conn = mysqli_connect("localhost", "mess_incidencias", "Pipmytrade123", "mess_rrhh");
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-mysqli_set_charset($conn, "utf8mb4");
-?>
